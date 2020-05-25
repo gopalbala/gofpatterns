9e1fe1c03746385782cd46e8caed550fd10c24f8
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-05-25 18:04:43 +0530

    changes during recording

--- src/main/java/com/gb/gofpatterns/structural/proxy/ProxyFileLoaderMain.java
+++ src/main/java/com/gb/gofpatterns/structural/proxy/ProxyFileLoaderMain.java
@@ -6,8 +6,11 @@ public class ProxyFileLoaderMain {
         File file1 = proxyFileService.getFile("file1.txt");
         File file2 = proxyFileService.getFile("file2.txt");
 
+        System.out.println("Contents of file 1");
         System.out.println(file1.getContent());
+        System.out.println("Contents of file 2");
         System.out.println(file2.getContent());
+        System.out.println("Contents of file 1");
         File file3 = proxyFileService.getFile("file1.txt");
         System.out.println(file3.getContent());
     }
