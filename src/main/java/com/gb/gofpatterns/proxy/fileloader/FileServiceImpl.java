0d2137ca835a56e04c19450672cdbeb075986afa
gopalbala <balasubramaniangopalakrishnan@gmail.com>
2020-05-25 13:54:25 +0530

    proxy pattern main class and resources added

--- src/main/java/com/gb/gofpatterns/proxy/fileloader/FileServiceImpl.java
+++ src/main/java/com/gb/gofpatterns/proxy/fileloader/FileServiceImpl.java
@@ -1,9 +1,11 @@
 package com.gb.gofpatterns.proxy.fileloader;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileServiceImpl implements FileService {
 
@@ -11,13 +13,16 @@ public class FileServiceImpl implements FileService {
     public File getFile(String name) {
         System.out.println("Loading file from disk " + name);
         try {
-            String content = Files.readString(Path.of(name), StandardCharsets.UTF_8);
+            URL res = getClass().getClassLoader().getResource(name);
+            String content = Files.readString(Paths.get(res.toURI()), StandardCharsets.UTF_8);
             File file = new File();
             file.setContent(content);
             file.setFileName(name);
             file.setPath("./");
             return file;
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
             e.printStackTrace();
         }
         return null;
